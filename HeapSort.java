0ae0aee02076db96e635d238f0eb181d76ba199c
Digbijoy08 <83782347+Digbijoy08@users.noreply.github.com>
2021-10-06 11:35:52 +0530

    Update HeapSort.java
    Here I have made some changes, Please go through it. Object Creation of HeapSort.

--- HeapSort.java
+++ HeapSort.java
@@ -1,16 +1,19 @@
+
+
 public class HeapSort {
 	public void sort(int arr[])
 	{
 		int n = arr.length;
 
 		
-		for (int i = n / 2 - 1; i >= 0; i--)
+		for (int i = n / 2 - 1; i >= 0; i--)    // Edited by Digbijoy Dasgupta
 			heapify(arr, n, i);
 
 		
-		for (int i = n - 1, i > 0; i--) {
+		for (int i = n - 1; i > 0; i--) {
 			
 			int temp = arr[0];
+			arr[0] = arr[i];       //Edited by Digbijoy Dasgupta
 			arr[i] = temp;
 
 			
@@ -21,22 +24,22 @@ public class HeapSort {
 	
 	void heapify(int arr[], int n, int i)
 	{
-		int largest = i
+		int largest = i;         //Edited by Digbijoy Dasgupta 
 		int l = 2 * i + 1; 
-		
+		int r = 2 * i + 2;       //Edited by Digbijoy Dasgupta
 
 		
 		if (l < n && arr[l] > arr[largest])
 			largest = l;
 
 		
-		if (r < n & arr[r] > arr[largest])
+		if (r < n && arr[r] > arr[largest])   //Edited by Digbijoy Dasgupta
 			largest = r;
 
 		
 		if (largest != i) {
 			int swap = arr[i];
-			
+			arr[i] = arr[largest];       //Edited by Digbijoy Dasgupta
 			arr[largest] = swap;
 
 			
@@ -49,17 +52,18 @@ public class HeapSort {
 	{
 		int n = arr.length;
 		for (int i = 0; i < n; ++i)
-		
+			System.out.print(arr[i] + " ");         //Edited by Digbijoy Dasgupta
 		System.out.println();
 	}
 
 	
 	public static void main(String args[])
 	{
-		int arr[] = { 12, 11, 13, 5, 6, 7 }
-		int n = arr.length;
+		int arr[] = { 12, 11, 13, 5, 6, 7 };             //Edited by Digbijoy Dasgupta
+		int n = arr.length;    
 
-		
+		HeapSort ob = new HeapSort();             //Edited by Digbijoy Dasgupta
+		ob.sort(arr);                            //Edited by Digbijoy Dasgupta
 
 		System.out.println("Sorted array is");
 		printArray(arr);
